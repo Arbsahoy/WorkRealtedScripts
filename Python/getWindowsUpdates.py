b631df27b208ebd56eff6c34086b865788643e30
Arbsahoy <82701320+Arbsahoy@users.noreply.github.com>
2023-03-30 10:03:16 -0600

    I got the response I am looking for.

--- Python/getWindowsUpdates.py
+++ Python/getWindowsUpdates.py
@@ -1,19 +1,37 @@
 import importlib.util as iutil
 import os
-rqts = ['requests', 'bs4']
+
+rqts = ['requests', 'selenium']
+
 print('Checking for required modules...')
+
 for r in rqts:
     if iutil.find_spec(r) is None:
-        print('Installing ' + r)
+        print('Installing ' + r + '.')
         os.system('! pip install ' + r)
     else:
-        print(r + ' is installed')
+        print(r + ' is installed.')
 
 import requests
 import json
 import time as t
-from bs4 import BeautifulSoup as bs4
+from selenium import webdriver
+from selenium.webdriver.common import keys
 
-r = requests.get('https://www.catalog.update.microsoft.com/Search.aspx?q=windows%2010%20cumulative%20update')
+driver = webdriver.Edge()
 
-print(r.status_code)
+driver.maximize_window()
+driver.get('https://www.catalog.update.microsoft.com/Search.aspx?q=windows%2010%20cumulative%20update')
+
+
+
+
+
+
+
+
+
+
+
+
+driver.close()
